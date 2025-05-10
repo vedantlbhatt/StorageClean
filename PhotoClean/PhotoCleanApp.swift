be577a9d521155e1f739140a89ca58126de5ac43
vedantlbhatt <vedubhatt2006@gmail.com>
2025-05-10 22:34:07 +0200

    Initial Commit

---
+++ PhotoClean/PhotoCleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCleanApp.swift
+//  PhotoClean
+//
+//  Created by Vedant Bhatt on 5/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoCleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoCleanTests/PhotoCleanTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCleanTests.swift
+//  PhotoCleanTests
+//
+//  Created by Vedant Bhatt on 5/10/25.
+//
+
+import Testing
+@testable import PhotoClean
+
+struct PhotoCleanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
